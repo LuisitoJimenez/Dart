1fdae765684d2d9d9493d2302adf0ac4a9574d13
Luis Jimenez <luis.jimenez@qualisolutions.tech>
2023-05-21 22:26:58 -0600

    Agregar codigo a los archivos .dart

---
+++ almacenamiento_imagenes/lib/dbManager.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+import 'dart:io' as io;
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'photo.dart';
+
+class dbManager {
+
+    static Database? _db;
+    static const String ID = 'id';
+    static const String PHOTO_NAME = 'photo_name';
+    static const String TABLE = 'photosTable';
+    static const String DB_NAME = "photos.db";
+
+    Future<Database> get db async {
+        if (_db != null) {
+            return _db!;
+        }
+        _db = await initDb();
+        return _db!;
+    }
+
+    initDb() async {
+        io.Directory documentsDirectory = await getApplicationDocumentsDirectory();
+        String path = join(documentsDirectory.path, DB_NAME);
+        var db = await openDatabase(path, version: 1, onCreate: _onCreate);
+        return db;
+    }
+
+    _onCreate(Database db, int version) async {
+        await db.execute("CREATE TABLE $TABLE ($ID INTEGER PRIMARY KEY, $PHOTO_NAME TEXT)");
+    }
+
+    Future<Photo> save(Photo photo) async {
+        var dbClient = await db;
+        photo.id = await dbClient.insert(TABLE, photo.toMap());
+        return photo;
+    }
+
+    Future<List<Photo>> getPhotos () async {
+        var dbClient = await db;
+        //List<Map> maps = await dbClient.query(TABLE, columns: [ID, PHOTO_NAME], where: '$ID = ?', whereArgs: [1]);
+        List<Photo> photos = [];
+        List<Map> maps = await dbClient.query(TABLE, columns: [ID, PHOTO_NAME]);
+        if (maps.isNotEmpty) {
+            for (int i = 0; i < maps.length; i++) {
+                photos.add(Photo.fromMap(maps[i] as Map<String, dynamic>));
+            }
+        }
+        return photos;
+    }
+
+}
