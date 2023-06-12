39c830eda700b28ce3605cd4ce15bc17b0f9dba0
Luis Jimenez <luis.jimenez@qualisolutions.tech>
2023-06-12 09:42:44 -0600

    Agregar proyecto

---
+++ reconocimiento_imagenes/lib/main.dart
@@ -0,0 +1,263 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:reconocimiento_imagenes/medicine.dart';
+import 'package:tflite/tflite.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: const imageRecognition(),
+    );
+  }
+}
+
+class imageRecognition extends StatefulWidget {
+  const imageRecognition({Key? key}) : super(key: key);
+
+  @override
+  State<imageRecognition> createState() => _imageRecognitionState();
+}
+
+class _imageRecognitionState extends State<imageRecognition> {
+  Map myMap = Map();
+  List<Medicine> medicine = [];
+  List? _outputs;
+  File? _image;
+  bool isLoading = false;
+
+  Future<Map<String, dynamic>> fetchData() async {
+    final response = await http.get(Uri.parse(
+        "https://raw.githubusercontent.com/LuisitoJimenez/Dart/02-Reconocimiento-imagenes-JSON/medicine.json"));
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      myMap = json.decode(response.body);
+      print("Download");
+      print(response.body);
+      Iterable i = myMap["Medicine"];
+      medicine = i.map((m) => Medicine.FromJson(m)).toList();
+    } else {
+      throw Exception("Failed to fetch data");
+    }
+    throw '';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    isLoading = true;
+    loadModel().then((value) {
+      setState(() {
+        isLoading = false;
+      });
+    });
+    fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Farmacia"),
+        centerTitle: true,
+      ),
+      body: isLoading
+          ? Container(
+              alignment: Alignment.center,
+              child: CircularProgressIndicator(),
+            )
+          : Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _image == null
+                      ? Container()
+                      : ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: Image.file(
+                            _image!,
+                            width: 200.0,
+                            height: 200.0,
+                          ),
+                        ),
+                  _outputs != null
+                      ? Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: Table(border: TableBorder.all(), children: [
+                            TableRow(children: [
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Nombre',
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  medicine[_outputs![0]["index"]]
+                                      .name
+                                      .toString(),
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontStyle: FontStyle.italic),
+                                ),
+                              ),
+                            ]),
+                            TableRow(children: [
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Dosis',
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  medicine[_outputs![0]["index"]]
+                                      .dosage
+                                      .toString(),
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontStyle: FontStyle.italic),
+                                ),
+                              ),
+                            ]),
+                            TableRow(children: [
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Descripción',
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  medicine[_outputs![0]["index"]]
+                                      .description
+                                      .toString(),
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontStyle: FontStyle.italic),
+                                ),
+                              ),
+                            ]),
+                            TableRow(children: [
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Contraindicación',
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  medicine[_outputs![0]["index"]]
+                                      .contraindication
+                                      .toString(),
+                                  style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontStyle: FontStyle.italic),
+                                ),
+                              ),
+                            ])
+                          ]))
+                      : Container()
+                ],
+              ),
+            ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: pickedImage,
+            child: const Icon(Icons.image),
+          ),
+          /*SizedBox(width: 16), // Espacio entre los botones
+          FloatingActionButton(
+            onPressed: pickedImageCamera,
+            child: const Icon(Icons.camera_alt),
+          ),*/
+        ],
+      ),
+    );
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: "assets/model_unquant.tflite", labels: "assets/labels.txt");
+  }
+
+  pickedImage() async {
+    final ImagePicker _picker = ImagePicker();
+    var image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image == null) {
+      return null;
+    }
+    setState(() {
+      isLoading = true;
+      _image = File(image.path.toString());
+    });
+    classifyImage(File(image.path));
+  }
+
+  pickedImageCamera() async {
+    final ImagePicker _picker = ImagePicker();
+    var image = await _picker.pickImage(
+        source: ImageSource.camera); // Cambiar a ImageSource.camera
+    if (image == null) {
+      return null;
+    }
+    setState(() {
+      isLoading = true;
+      _image = File(image.path.toString());
+    });
+    classifyImage(File(image.path));
+  }
+
+  classifyImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+        path: image.path,
+        numResults: 5,
+        threshold: 0.5,
+        imageMean: 127.5,
+        imageStd: 127.5);
+    setState(() {
+      isLoading = false;
+      _outputs = output!;
+      print(_outputs);
+      print(_outputs![0]["index"]);
+    });
+  }
+
+  void dispose() {
+    Tflite.close();
+    super.dispose();
+  }
+}
---
+++ reconocimiento_imagenes/lib/medicine.dart
@@ -0,0 +1,30 @@
+
+class Medicine {
+
+  String? id;
+  String? name;
+  String? dosage;
+  String? description;
+  String? contraindication;
+
+  Medicine({required this.id, required this.name, required this.dosage, required this.description, required this.contraindication});
+
+  Medicine.FromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    dosage = json['dosage'];
+    description = json['description'];
+    contraindication = json['contraindication'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['dosage'] = this.dosage;
+    data['description'] = this.description;
+    data['contraindication'] = this.contraindication;
+    return data;
+  }
+
+}
